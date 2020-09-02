4bf8347d1d883893aaffa5fd97542b7d88d5d251
DaniAkash <s.daniakash@gmail.com>
2020-09-03 05:01:01 +0530

    chore: install storybook web

---
+++ example/.storybook/webpack.config.js
@@ -0,0 +1,6 @@
+const { resolve } = require('path');
+const { withUnimodules } = require('@expo/webpack-config/addons');
+
+module.exports = ({ config }) => {
+  return withUnimodules(config, { projectRoot: resolve(__dirname, '../') });
+};
